b35e04667f0ff2fd1843e3fd0e14d86177a69602
Maggie Hollis <s013332@students.lmsd.org>
2020-04-22 19:33:09 -0400

    just added a tonnnn of comments

--- Tierodactyl/TierViewController.swift
+++ Tierodactyl/TierViewController.swift
@@ -12,10 +12,13 @@ import UIKit
 
 class TierViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UICollectionViewDragDelegate, UICollectionViewDropDelegate {
    
+    //keeps track of the number of sections
     var secCount = 0
+    //this holds the number of cells as well as the text to go with each cell
     var words = [[UILabel]()]
-    var cleared = false
+   
     
+    //creating the collection view, adding some basc formatting, more later
     let collection: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 16
@@ -34,16 +37,12 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
         longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(self.longTap(_:)))
         collection.addGestureRecognizer(longPressGesture)
         
-        
         setUpViews()
         
-        if !cleared {
-            words.removeAll()
-            cleared = true
-        }
     
     }
     
+    //adds a bar to the bottom of the screen that allows the user to add a tier or a cell
     override func viewWillLayoutSubviews() {
        let width = self.view.frame.width
         let height = self.view.frame.height
@@ -52,14 +51,17 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
        let navigationBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: height - 75, width: width, height: 75))
        self.view.addSubview(navigationBar)
        let navigationItem = UINavigationItem(title: "Navigation bar")
+        //adds a button to bar, this is the plus bottom that calls addSect and pops some alerts up
        let doneBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: nil, action: #selector(addSect))
        navigationItem.rightBarButtonItem = doneBtn
         navigationItem.title = "add a tier or cell"
  
+        //tells the bar to show the button
        navigationBar.setItems([navigationItem], animated: false)
         
     }
     
+    //sets up the collection view, sets its delegates and datasources, registers its cell
     func setUpViews(){
         collection.delegate = self
         collection.dataSource = self
@@ -69,8 +71,10 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
         
         collection.register(firstCell.self, forCellWithReuseIdentifier: "cell")
        
+        //this is a header that will be above each section
         collection.register(SectionHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader , withReuseIdentifier: "header")
         
+        //constraints for the collection view
         view.addSubview(collection)
         collection.translatesAutoresizingMaskIntoConstraints = false
         collection.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
@@ -79,35 +83,43 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
         collection.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
     
+    //height of the section headers
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: 60)
     }
     
    
+    //this adds text to section headers and this is where you can change their color
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
       
+        //making sure that what is being edited is the header
         if kind == UICollectionView.elementKindSectionHeader {
             let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath) as! SectionHeader
             sectionHeader.label.text = String(indexPath.section + 1)
             sectionHeader.backgroundColor = .gray
-            
-
         
              return sectionHeader
-        } else { //No footer in this case but can add option for that
+        }
+            
+        else { //No footer in this case but can add option for that
              return UICollectionReusableView()
         }
     }
 
 
+    //number of sections in the collectionview
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return secCount
     }
     
+    //number of cells in each section/tier
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return words[section].count
     }
     
+    //this just sets up the basic cell
+    //they are of type firstCell which does not have much of its own functionality
+    //also adds whatever label goes with it
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! firstCell
@@ -118,6 +130,7 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
         return cell
     }
     
+    //this customzes labels that go on the cell and allows the text to be more than one line, formats label
     func setUpLabel(_ indexPath: IndexPath, _ cell: firstCell){
         words[indexPath.section][indexPath.row].lineBreakMode = .byWordWrapping // notice the 'b' instead of 'B'
         words[indexPath.section][indexPath.row].numberOfLines = 0
@@ -125,11 +138,13 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
         words[indexPath.section][indexPath.row].font = UIFont(name: "Times New Roman", size: 15)
     }
     
+    //this is where you change the cell sizes
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 
         return CGSize(width: (view.frame.width / 3) - 16, height: 100)
     }
     
+    //this is the space around the sections, how far from the edge and other sections a tier is
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 10, left: 0, bottom: 30, right: 0)
     }
@@ -137,29 +152,37 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
     //adding text/editing a cell
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
        
+        //an alert pops up when you click on a cell
         let alert = UIAlertController(title: "Edit Cell", message: "Enter the text you would like to add to this cell", preferredStyle:
                    UIAlertController.Style.alert)
 
+        //text field which allows the user to add or edit the text of a cell
                alert.addTextField(configurationHandler: textFieldHandler)
+        //adds and pre exisiting text to the text box so they can change it without re typing it
                alert.textFields?.first!.text = words[indexPath.section][indexPath.row].text
 
+        //does nothing, just closes the alert
                alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler:{ (UIAlertAction) in
                    
                }))
-
+        //this adds the text
                alert.addAction(UIAlertAction(title: "Done", style: UIAlertAction.Style.default, handler:{ (UIAlertAction) in
                 
+                //clears the view so no overlappign labels
                     collectionView.cellForItem(at: indexPath)?.contentView.subviews.forEach({ $0.removeFromSuperview() })
-                 
+                 //creates a variable for the cell, shorter and easier to use later
                     let cell = collectionView.cellForItem(at: indexPath) as! firstCell
                 
+                //a stand in label that will be used to put the text on the cell
                     let label = UILabel()
                     label.text = (alert.textFields?.first!.text)!
                         
+                //label formatting
                     label.font = UIFont(name: "Times New Roman", size: 30)
                     label.textColor = .black
                     label.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
                         
+                //adds the label to the cell and then reloads everything
                     self.words[indexPath.section][indexPath.row] = label
                     collectionView.reloadData()
     
@@ -168,14 +191,10 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
             self.present(alert, animated: true, completion:nil)
     }
     
-    func textFieldHandler(textField: UITextField!) {
-        if (textField) != nil {
-               textField.text = ""
-        }
-    }
-    
-    
+ 
     //drag and drop stuff
+    //this is when you are dropping the cell into its new place
+    //it finds the desitination and makes it the new index path for the cell unless the destination is nil
     
     func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
            var destinationIndexPath: IndexPath
@@ -183,11 +202,14 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
            if let indexPath = coordinator.destinationIndexPath{
                destinationIndexPath = indexPath
            }
+            //if nil then it places at the end of whatever section the cell is above
            else{
             let row = collectionView.numberOfItems(inSection: 0)
+            //if having issue with dropping on end, maybe remove "-1"
                destinationIndexPath = IndexPath(row: row - 1, section: 0)
            }
            
+        //after the move is made it calls reorderItems to rearrange the cells and sort everything out
            if coordinator.proposal.operation == .move{
                self.reorderItems(coordinator: coordinator, destinationIndexPath: destinationIndexPath, collectionView: collectionView)
            
@@ -201,18 +223,22 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
           
             collectionView.performBatchUpdates({
                
+                //moves the cell from one location to the other
                 collectionView.moveItem(at: sourceIndexPath, to: destinationIndexPath)
                 
+                //moves the labels from the old cell to the new one
                 words[destinationIndexPath.section].insert(words[sourceIndexPath.section][sourceIndexPath.row], at: destinationIndexPath.row)
                 words[sourceIndexPath.section].remove(at: sourceIndexPath.row)
                 
                 }, completion: nil)
             
+            //finally, the drop is completed
              coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
         }
         
     }
     
+    //makes sure the drop is complete and if it isnt, this completes it
     func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
         if collectionView.hasActiveDrag{
             return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
@@ -221,9 +247,11 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
         return UICollectionViewDropProposal(operation: .forbidden)
     }
     
+    //identifies what is being dragged
     func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
         
-           let item = String(indexPath.row + 1)
+        //testing something new out, might now work
+           let item = words[indexPath.section][indexPath.row]//String(indexPath.row + 1)
            let itemProvider = NSItemProvider(object: item as NSString)
            let dragItem = UIDragItem(itemProvider: itemProvider)
            dragItem.localObject = item
@@ -231,26 +259,31 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
        }
     
   
+    //this is the functionality of the bottom on the bottom of the screen
      @objc func addSect(){
       
+        //an alert will pop up
         let alert = UIAlertController(title: "Edit Cell", message: "", preferredStyle:
                           UIAlertController.Style.alert)
 
-                    //  alert.addTextField(configurationHandler: textFieldHandler)
-
+                 
+        //adds a section with one cell, updates secCount to have the right number of sections
+        //reloads everything
                       alert.addAction(UIAlertAction(title: "Add Tier", style: UIAlertAction.Style.default, handler:{ (UIAlertAction) in
                         self.secCount+=1
-                        self.words.append([UILabel(),UILabel()])
+                        self.words.append([UILabel()})
                         self.collection.reloadData()
                            
                       }))
 
+    //if they want to add a cell, this alert will close and another will open (in the func addCell)
                       alert.addAction(UIAlertAction(title: "Add Cell", style: UIAlertAction.Style.default, handler:{ (UIAlertAction) in
                          
                         self.addCell()
                       
                       }))
         
+    //does nothing, closes alert
                     alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler:{ (UIAlertAction) in
 
                     }))
@@ -259,17 +292,22 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
         
         }
     
+//if the user wants to add a cell (selected above) this function is called
     func addCell(){
-        let alert = UIAlertController(title: "Edit Cell", message: "Enter the teir you would like to add a cell to", preferredStyle:
-                                        UIAlertController.Style.alert)
+        let alert = UIAlertController(title: "Edit Cell", message: "Enter the teir you would like to add a cell to", preferredStyle: UIAlertController.Style.alert)
 
+        //text box so they can say which section they want the cell to be in
             alert.addTextField(configurationHandler: textFieldHandler)
 
+        //this is called when they click add cell
             alert.addAction(UIAlertAction(title: "Add Cell", style: UIAlertAction.Style.default, handler:{ (UIAlertAction) in
                
+                //makes sure that the number entered into the textboc is a valid integer
                 guard let index = Int((alert.textFields?.first!.text)!) else {return}
                 
+                //makes sure a corresponding section exists
                 if index <= self.words.count{
+                    //adds a label to words to account for one more cell and reloads everything
                     self.words[index-1].append(UILabel())
                     self.collection.reloadData()
                 }
@@ -370,6 +408,7 @@ class TierViewController: UIViewController, UICollectionViewDelegate, UICollecti
     
 }
 
+//a class for the cells, just changes background color and gives a lil curve to the corners
 class firstCell: UICollectionViewCell{
 
     override init(frame: CGRect) {
@@ -385,7 +424,9 @@ class firstCell: UICollectionViewCell{
 
 }
 
+//this is the class for the header above each section
 class SectionHeader: UICollectionReusableView {
+    //creates the label that the headers have to say which section it is, formats it
      var label: UILabel = {
      let label: UILabel = UILabel()
      label.textColor = .white
@@ -394,6 +435,7 @@ class SectionHeader: UICollectionReusableView {
      return label
  }()
 
+    //provides constraints for the label on the header and adds the label to the headers view so it will show up
 override init(frame: CGRect) {
      super.init(frame: frame)
 
