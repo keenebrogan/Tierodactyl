81c6e12a91f7dd548de586406a818253dabcfd07
Maggie Hollis <s013332@students.lmsd.org>
2020-05-08 18:06:30 -0400

    unwind segue!

--- Tierodactyl/HomeScreenViewController.swift
+++ Tierodactyl/HomeScreenViewController.swift
@@ -189,7 +189,7 @@ class HomeScreenViewController: UIViewController, UITableViewDelegate, UITableVi
         } catch let signOutError as NSError {
           print ("Error signing out: %@", signOutError)
         }
-        self.performSegue(withIdentifier: "SignOut", sender: nil)
+        self.performSegue(withIdentifier: "unwindd", sender: nil)
 
     }
     
